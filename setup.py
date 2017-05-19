e4bdeebf3b43436592e214623a940ef897d66801
Jakab Gabor <jkbgbr@gmail.com>
2017-05-19 10:05:32 +0200

    added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Srep3',
+    version='0.1',
+    packages=['tests', 'geometry', 'SR_Vessel'],
+    url='https://github.com/jkbgbr/Srep3',
+    license='GPLv3',
+    author='Jakab Gabor',
+    author_email='jkbgbr@gmail.com',
+    description='2D polygon representation of 3D objects'
+)
